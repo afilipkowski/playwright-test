b9620bcc3ca60b2d6cb656c9367fbf91ec4bfa4a
Aleksander <filipkowskialek@gmail.com>
2023-10-31 14:25:05 +0100

    Minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 // pages/index.js
 import Link from 'next/link';
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Playwright Test</title>
+      </Head>
       <h1>Home Page</h1>
       <Link href="/subpage">
         Go to Subpage
--- src/pages/subpage.js
+++ src/pages/subpage.js
@@ -1,9 +1,13 @@
 // pages/subpage.js
 import Link from 'next/link';
+import Head from 'next/head'
 
 export default function Subpage() {
   return (
     <div>
+      <Head>
+        <title>Playwright Test</title>
+      </Head>
       <h1>Subpage</h1>
       <Link href="/">
         Go to Home
